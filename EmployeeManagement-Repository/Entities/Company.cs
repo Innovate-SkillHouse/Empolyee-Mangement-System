ac1280ec7170dcf0c0b4f8dabc046cd0d2819895
Vishal <vishal@aykansoft.com>
2022-06-01 19:57:05 +0530

    New changes

---
+++ EmployeeManagement-Repository/Entities/Company.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EmployeeManagement_Repository.Entities
+{
+    public partial class Company
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public string Address { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ EmployeeManagement-Repository/Entities/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EmployeeManagement_Repository.Entities
+{
+    public partial class Project
+    {
+        public int Id { get; set; }
+        public string ProjectName { get; set; }
+        public string Duration { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
