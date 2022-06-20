686167cea31f81f8c8c6ea4fd1683de66e070b57
Vishal <vishal@aykansoft.com>
2022-06-20 12:07:44 +0530

    Context file changes

--- EmployeeManagement-Repository/Entities/EmployeeManagementContext.cs
+++ EmployeeManagement-Repository/Entities/EmployeeManagementContext.cs
@@ -29,7 +29,7 @@ namespace EmployeeManagement_Repository.Entities
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=LAPTOP-E37B9H3T\\SQLEXPRESS01;Database=EmployeeManagement;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=.;Database=EmployeeManagement;Trusted_Connection=True;");
             }
         }
 
